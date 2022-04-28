5470aca7d73c7d9dd0c7d9cf8ae9b5f727321702
DerrickAmoateng <99464972+DerrickAmoateng@users.noreply.github.com>
2022-04-28 21:02:56 +0000

    add files to repo

---
+++ modules/src/app/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import { Inventory } from '../features/inventory/Inventory.js';
+import { CurrencyFilter } from '../features/currencyFilter/CurrencyFilter.js';
+// Import the Cart component here.
+import { Cart } from  '../features/cart/Cart.js';
+// Render the Cart component below <Inventory />
+
+import { SearchTerm } from '../features/searchTerm/SearchTerm.js';
+
+export const App = (props) => {
+
+  const { state, dispatch } = props;
+  const visibleInventory = getFilteredInventory(state.inventory, state.searchTerm)
+
+  return (
+    <div>
+      <CurrencyFilter
+        currencyFilter={state.currencyFilter}
+        dispatch={dispatch}
+      />
+      
+      <SearchTerm
+        searchTerm={state.searchTerm}
+        dispatch={dispatch}
+      />
+
+      <Inventory
+        inventory={visibleInventory}
+        currencyFilter={state.currencyFilter}
+        dispatch={dispatch}
+      />
+
+      <Cart
+        cart={state.cart}
+        currencyFilter={state.currencyFilter}
+        dispatch={dispatch} />
+
+    </div>
+  );
+};
+
+function getFilteredInventory(inventory, searchTerm) {
+  return inventory.filter(items => items.name.toLowerCase().includes(searchTerm.toLowerCase()));
+}
